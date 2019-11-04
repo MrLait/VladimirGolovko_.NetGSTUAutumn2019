dfa6801742af6af48bef9db08a8f8e6d8e88621f
Lait <mr.lait3@gmail.com>
2019-11-04 17:03:11 +0300

    Задание. (Не выполнено проверять не нужно) + Разработать тип, реализующий алгоритм Евклида для вычисления НОД двух целых чисел (http://en.wikipedia.org/wiki/Euclidean_algorithm). + Добавить к разработанному типу дополнительную функциональность в виде перегруженных методов вычисления НОД для трех, четырех или пяти целых чисел. + Добавить к разработанному типу метод, реализующий алгоритм Стейна (бинарный алгоритм Эвклида) для расчета НОД двух целых чисел (http://en.wikipedia.org/wiki/Binary_GCD_algorithm). Метод должен принимать выходной параметр, содержащий значение времени, затраченное для выполнения расчетов. - Изменить метод, реализующий алгоритм Евклида вычисления НОД двух параметров, добавив аналогичную функциональность. - Дополнить методом, подготавливающим данные для построения гистограммы, сравнивающей время нахождения решения каждым из методов. - Создать unit-тесты для тестирования разработанных методов. - Весь код должен быть снабжён комментариями (автокомментариями)

---
+++ task01_NET/ConsoleApp1/ConsoleApp1/EuclideanAlgorithm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class EuclideanAlgorithm
+    {
+        public static int FindGCD(int firstNum, int secondNum)
+        {
+            return Math.Abs(firstNum == 0 ? secondNum : FindGCD(secondNum % firstNum, firstNum));
+        }
+        public static Int32 FindGCD(Int32 firstNum, Int32 secondNum, Int32 thirdNum)
+        {
+            Int32 GCDOfTwoNumbers = FindGCD(firstNum, secondNum);
+            return GCDOfTwoNumbers == 0 ? thirdNum : FindGCD(thirdNum % GCDOfTwoNumbers, GCDOfTwoNumbers);
+        }
+        public static Int32 FindGCD(Int32 firstNum, Int32 secondNum, Int32 thirdNum, Int32 fourthNum)
+        {
+            Int32 GCDOfThirdNumbers = FindGCD(firstNum, secondNum, thirdNum);
+            return GCDOfThirdNumbers == 0 ? fourthNum : FindGCD(fourthNum % GCDOfThirdNumbers, GCDOfThirdNumbers);
+        }
+
+        public static int FindBinaryGCD(int a, int b)
+        {
+            if (a == b)
+                return a;
+            if (a == 0)
+                return b;
+            if (b == 0)
+                return a;
+            if ((~a & 1) != 0)
+            {
+                if ((b & 1) != 0)
+                    return FindBinaryGCD(a >> 1, b);
+                else
+                    return FindBinaryGCD(a >> 1, b >> 1) << 1;
+            }
+            if ((~b & 1) != 0)
+                return FindBinaryGCD(a, b >> 1);
+            if (a > b)
+                return FindBinaryGCD((a - b) >> 1, b);
+            return FindBinaryGCD((b - a) >> 1, a);
+        }
+
+    }
+}
--- task01_NET/ConsoleApp1/ConsoleApp1/Program.cs
+++ task01_NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(78, 294));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(5, 0));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(64, -48));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(-78, 294, 570, 36));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(78, 294, 590, 38));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(-231, -140));
+            Console.WriteLine(EuclideanAlgorithm.FindGCD(-585, 81,-189));
+            Console.WriteLine();
+            Console.WriteLine(EuclideanAlgorithm.FindBinaryGCD(78,294));
+            Console.WriteLine(EuclideanAlgorithm.FindBinaryGCD(64, 48));
         }
     }
 }
