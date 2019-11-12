891a18b643cd5050a1cdbe151ea762fa58b425df
Lait <mr.lait3@gmail.com>
2019-11-12 15:54:19 +0300

    The Vector class is created. Implemented operator overloading:  -, !=, ==, +, *multiplies a vector by a number, / divides a vector by a number. Unit tests added.

---
+++ task02_NET/OverloadOperationsTask1/OverloadOperationsTask1/Vector.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace OverloadOperationsTask1
+{
+    public class Vector
+    {
+        private readonly double x;
+        private readonly double y;
+        private readonly double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public double X => x;
+        public double Y => y;
+        public double Z => z;
+
+        /// <summary>
+        /// operator -Subtracts one vector from another.
+        /// </summary>
+        /// <param name="v1">The first vector.</param>
+        /// <param name="v2">The second vector.</param>
+        /// <returns>  </returns>
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(
+                v1.X - v2.X, 
+                v1.Y - v2.Y, 
+                v1.Z - v2.Z);
+        }
+        //operator == Returns true if two vectors are approximately equal.
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return ((v1.X == v2.X) &&
+                    (v1.Y == v2.Y) &&
+                    (v1.Z == v2.Z));
+        }
+        //operator != Returns true if vectors different.
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+
+        //operator +Adds two vectors.
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(
+                v1.X + v2.X, 
+                v1.Y + v2.Y, 
+                v1.Z + v2.Z);
+        }
+        //operator *Multiplies a vector by a number.
+        public static Vector operator *(Vector v1, double d1)
+        {
+            return new Vector(
+                v1.X * d1,
+                v1.Y * d1,
+                v1.Z * d1);
+        }
+        public static Vector operator *(double d1, Vector v1)
+        {
+            return new Vector(
+                v1.X * d1,
+                v1.Y * d1,
+                v1.Z * d1);
+        }
+
+        //operator / Divides a vector by a number.
+        public static Vector operator /(Vector v1, double d1)
+        {
+            return new Vector(
+                v1.X / d1,
+                v1.Y / d1,
+                v1.Z / d1);
+        }
+        public static Vector operator /(double d1, Vector v1)
+        {
+            return new Vector(
+                v1.X / d1,
+                v1.Y / d1,
+                v1.Z / d1);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Vector)
+            {
+                Vector v = (Vector)obj;
+                return ((this.X == v.X) &&
+                        (this.Y == v.Y) &&
+                        (this.Z == v.Z));
+            }
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            return 33 * X.GetHashCode() + Y.GetHashCode()
+                + Z.GetHashCode();
+        }
+
+    }
+}
---
+++ task02_NET/OverloadOperationsTask1/OverloadOperationsTask1Tests/VectorTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OverloadOperationsTask1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperationsTask1.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [TestMethod()]
+        public void VectorOperatorsTest()
+        {
+            Vector vector1 = new Vector(1, -1, 2);
+            Vector vector2 = new Vector(1, 1, 1);
+
+            //operator -Subtracts one vector from another.
+            Vector actual = vector1 - vector2;
+            Vector expected = new Vector(0, -2, 1);
+            Assert.AreEqual(expected, actual);
+
+            //operator != Returns true if vectors different.
+            bool actualBool = vector1 != vector2;
+            Assert.IsTrue(actualBool);
+
+            //operator == Returns true if two vectors are approximately equal.
+            actualBool = vector1 == new Vector(1, -1, 2);
+            Assert.IsTrue(actualBool);
+
+            //operator +Adds two vectors.
+            actual = vector1 + vector2;
+            expected = new Vector(2, 0, 3);
+            Assert.AreEqual(expected, actual);
+
+            //operator *Multiplies a vector by a number.
+            actual = vector1 * 5;
+            expected = new Vector(5, -5, 10);
+            Assert.AreEqual(expected, actual);
+
+            actual = 5 * vector1;
+            Assert.AreEqual(expected, actual);
+
+            //operator / Divides a vector by a number.
+            actual = vector1 / 5;
+            expected = new Vector(0.2, -0.2, 0.4);
+            Assert.AreEqual(expected, actual);
+            actual = 5 / vector1;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
