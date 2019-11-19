86838ca20370fceb813cc113c546213a7f782d38
Lait <mr.lait3@gmail.com>
2019-11-19 14:41:06 +0300

    Added overriding of the division operator and tests to verify the method.

---
+++ src/Task02/OverloadOperationsTask2/Polinom.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverloadOperationsTask2
+{
+    public class Polinom
+    {
+        public double[] Quotient { get; set; }
+        public double[] Remainder { get; set; }
+        public double[] PolinomParams { get; set; }
+
+       public  Polinom(double[] polinomParams )
+        {
+            PolinomParams = polinomParams;
+        }
+
+        public Polinom(double[] quotient, double[] remainder)
+        {
+            Quotient = quotient;
+            Remainder = remainder;
+        }
+
+        /// <summary>
+        /// The method of dividing a polynomial into a polynomial.
+        /// Where polinom index is degree.
+        /// </summary>
+        /// <param name="dividend">Parameters of a divisible polynomial.</param>
+        /// <param name="divider">Polynomial divisor coefficients.</param>
+        /// <param name="quotient">Parameters of a quotient polynomial.</param>
+        /// <param name="remainder">Parameters of a remainder polynomial.</param>
+        public static Polinom operator /(Polinom dividend, Polinom divider)
+        {
+            double[] dividendParams = dividend.PolinomParams;
+            double[] dividerParams = divider.PolinomParams;
+
+            if (dividend.PolinomParams.Length < divider.PolinomParams.Length)
+                throw new ArithmeticException("The degree of the divisor must be less than or equal" +
+                    " to the degree of the dividend!");
+            if (dividend.PolinomParams[dividend.PolinomParams.Length-1] == 0 
+                | divider.PolinomParams[divider.PolinomParams.Length - 1] == 0)
+            {
+                throw new ArgumentException("The Last polinom parameters can't be is zero");
+            }
+
+            double[] quotient = new double[dividendParams.Length - dividerParams.Length + 1];
+            double[] remainder = dividend.PolinomParams;
+
+            for (int i = 0; i < quotient.Length; i++)
+            {
+                double coeff = remainder[dividendParams.Length - i - 1] / dividerParams[dividerParams.Length - 1];
+                quotient[quotient.Length - 1 - i] = coeff;
+
+                for (int j = 0; j < dividerParams.Length; j++)
+                {
+                    remainder[dividendParams.Length - 1 - i - j] = remainder[dividendParams.Length - 1 - i - j]
+                        - dividerParams[dividerParams.Length - 1 - j] * coeff;
+                }
+            }
+
+            return new Polinom(quotient, remainder);
+        }
+    }
+}
---
+++ tests/Task02_Tests/OverloadOperationsTask2Tests/OverloadOperationsTask2Tests/PolinomTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OverloadOperationsTask2;
+
+namespace OverloadOperationsTask2Tests
+{
+    [TestClass]
+    public class PolinomTests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetDivisionForTwoPolinomWhenInputPolinomIsValidThenOutIsValid()
+        {
+            // Arrange
+            Polinom dividend = new Polinom(
+                //3x^5 + 2x^4 + 0x^3 + x^2 - x^1 + 1
+                polinomParams: new double[] { 1, -1, 1, 0, 2, 3 });
+            Polinom divider = new Polinom(
+                //x^3 + 2x^2 + x + 0
+                polinomParams: new double[] { 0, 1, 2, 1 });
+            
+            Polinom expected = new Polinom(
+                // quotient = 3x^2 -4x + 5,
+                quotient: new double[] { 5, -4, 3 },
+                //remainder = -5x^2 -6x + 1
+                remainder: new double[] { 1, -6, -5, 0, 0, 0 });
+
+            //Act
+            Polinom actual = dividend / divider;
+
+            // Assert
+            CollectionAssert.AreEqual(expected.Quotient, actual.Quotient);
+            CollectionAssert.AreEqual(expected.Remainder, actual.Remainder);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ExpectedException(typeof(ArgumentException),
+        "The Last polinom parameters can't be is zero")]
+        [TestMethod]
+        public void GetDivisionForTwoPolinomWhenInputPolinomWithFirstNumIsZeroThenOutIsArgumentException()
+        {
+            // Arrange
+            Polinom dividend = new Polinom(
+                //0x^5 + 2x^4 + 0x^3 + x^2 - x^1 + 1
+                polinomParams: new double[] { 1, -1, 1, 0, 2, 0 });
+            Polinom divider = new Polinom(
+                //x^3 + 2x^2 + x + 0
+                polinomParams: new double[] { 0, 1, 2, 1 });
+
+            Polinom expected = new Polinom(
+                // quotient = 0x^2 + 2x - 4,
+                quotient: new double[] { -4, 2, 0 },
+                //remainder = 7x^2 -3x + 1
+                remainder: new double[] { 1, 3, 7, 0, 0, 0 });
+
+            //Act
+            Polinom actual = dividend / divider;
+
+            // Assert
+            CollectionAssert.AreEqual(expected.Quotient, actual.Quotient);
+            CollectionAssert.AreEqual(expected.Remainder, actual.Remainder);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ExpectedException(typeof(ArithmeticException),
+        "The degree of the divisor must be less than or equal" +
+                    " to the degree of the dividend!")]
+        [TestMethod]
+        public void GetDivisionForTwoPolinomWhenFirstPolinomLessThenSecondThenOutIsArithmeticException()
+        {
+            // Arrange
+            Polinom dividend = new Polinom(
+                //0x^5 + 2x^4 + 0x^3 + x^2 - x^1 + 1
+                polinomParams: new double[] { 0, 1, 2, 1 });
+            Polinom divider = new Polinom(
+                //x^3 + 2x^2 + x + 0
+                polinomParams: new double[] { 1, -1, 1, 0, 2, 0 });
+
+            Polinom expected = new Polinom(
+                // quotient = 0x^2 + 2x - 4,
+                quotient: new double[] { -4, 2, 0 },
+                //remainder = 7x^2 -3x + 1
+                remainder: new double[] { 1, 3, 7, 0, 0, 0 });
+
+            //Act
+            Polinom actual = dividend / divider;
+
+            // Assert
+            CollectionAssert.AreEqual(expected.Quotient, actual.Quotient);
+            CollectionAssert.AreEqual(expected.Remainder, actual.Remainder);
+        }
+
+        [TestMethod]
+        public void GetDivisionForTwoValidPolinomWhenFirstPolinomMoreThenSecondThenOutRemainderIsZero()
+        {
+            // Arrange
+            Polinom dividend = new Polinom(
+                //2x^3 - 3x^2 + 5x^1 + 14
+                polinomParams: new double[] { -14, 5, -3, 2 });
+            Polinom divider = new Polinom(
+                //x -2
+                polinomParams: new double[] {-2, 1 });
+
+            Polinom expected = new Polinom(
+                // quotient = 0x^2 + 2x - 4,
+                quotient: new double[] { 7, 1, 2 },
+                //remainder = 7x^2 -3x + 1
+                remainder: new double[] { 0, 0, 0, 0 });
+
+            //Act
+            Polinom actual = dividend / divider;
+
+            // Assert
+            CollectionAssert.AreEqual(expected.Quotient, actual.Quotient);
+            CollectionAssert.AreEqual(expected.Remainder, actual.Remainder);
+        }
+
+        [ExpectedException(typeof(ArgumentException),
+        "The Last polinom parameters can't be is zero")]
+        [TestMethod]
+        public void GetDivisionForTwoValidPolinomWhenFirstPolinomMoreThenSecondThenOutIsArgumentException()
+        {
+            // Arrange
+            Polinom dividend = new Polinom(
+                //0x^3 - 1x^2 + 2x^1 + 3
+                polinomParams: new double[] { 3, 2, 1, 0 });
+            Polinom divider = new Polinom(
+                //0x^1 + 1
+                polinomParams: new double[] { 1, 0 });
+
+            Polinom expected = new Polinom(
+                // quotient = 0x^2 + 2x - 4,
+                quotient: new double[] { 7, 1, 2 },
+                //remainder = 7x^2 -3x + 1
+                remainder: new double[] { 0, 0, 0, 0 });
+
+            //Act
+            Polinom actual = dividend / divider;
+
+            // Assert
+            CollectionAssert.AreEqual(expected.Quotient, actual.Quotient);
+            CollectionAssert.AreEqual(expected.Remainder, actual.Remainder);
+        }
+
+    }
+}
